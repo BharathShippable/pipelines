06e2e698692e8dbb0d7a38e0cd51dccb72b1d627
Bharath Upadhya <1992bharath@gmail.com>
2018-03-21 21:19:33 +0530

    update resource name

--- startStopAwsInstances.sh
+++ startStopAwsInstances.sh
@@ -12,31 +12,19 @@ set_defaults() {
   if [ -z "$INSTANCE_TYPE" ]; then
     export INSTANCE_TYPE="t2.medium"
   fi
-}
-
-configure_creds() {
-  local pem_resource="bharath-us-east-1"
-  local pem_resource_upper_case=$(echo $pem_resource | awk '{print toupper($0)}')
-  local pem_resource_meta=$(eval echo "$"$pem_resource_upper_case"_META")
-  echo "Extracting AWS PEM"
-  echo "-----------------------------------"
-  pushd $pem_resource_meta
-  if [ ! -f "integration.json" ]; then
-    echo "No credentials file found at location: $RES_PEM_META"
-    return 1
+  if [ -z "$KEY_NAME" ]; then
+    export KEY_NAME="bharath-us-east-1"
   fi
-
-  cat integration.json | jq -r '.key' > "$PEM_KEY_LOCATION"
-  chmod 600 "$PEM_KEY_LOCATION"
-
-  echo "Completed Extracting AWS PEM"
-  echo "-----------------------------------"
-
-  popd
 }
 
 run_instance() {
-  aws ec2 run-instances --image-id "$AMI_ID" --count "$COUNT" --instance-type "$INSTANCE_TYPE" --key-name "$PEM_KEY_LOCATION"
+  echo "Running instance"
+  echo "-----------------------------------"
+
+  aws ec2 run-instances --image-id "$AMI_ID" --count "$COUNT" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME"
+
+  echo "Completed running instance"
+  echo "-----------------------------------"
 }
 
 main() {
@@ -46,7 +34,6 @@ main() {
     aws --version
     echo "---------------------------"
     if [ "$ACTION" == "run" ]; then
-      configure_creds
       run_instance
     else
       echo "Unknown ACTION: $ACTION"
