b6efa3bd1173086d3a6437b06d39d4c083395311
Bharath Upadhya <1992bharath@gmail.com>
2018-05-23 20:41:36 +0530

    run jobs for multiple nodepools

--- updateClusterRuntimeTemplateId.sh
+++ updateClusterRuntimeTemplateId.sh
@@ -1,21 +1,20 @@
 #!/bin/bash -e
 
 main() {
-  echo "=== GETting $subscriptionId's default cluster ==="
-  defaultClusterId=$(curl -H "Authorization: apiToken $apiToken" $apiUrl/subscriptions/$subscriptionId | jq '.defaultClusterId')
-  echo "$subscriptionId's default cluster is $defaultClusterId"
-  echo "=== Checking $subscriptionId's default cluster is dynamic ==="
-  is_dynamic=$(curl -H "Authorization: apiToken $apiToken" $apiUrl/clusters?subscriptionIds=$subscriptionId | jq '.[] | select(.id=='$defaultClusterId' and .clusterTypeCode=='$dynamicClusterType')')
-  if [ -z "$is_dynamic" ]; then
-    echo "Default cluster is not dynamic"
+  echo "=== GETting $nodePoolName's cluster ==="
+  cluster=$(curl -H "Authorization: apiToken $apiToken" $apiUrl/clusters?subscriptionIds=$subscriptionId | jq '.[] | select(.name="'$nodePoolName'" and .clusterTypeCode=='$dynamicClusterType')')
+  echo "node pool $nodePoolName's cluster is $clusterId"
+  if [ -z "$cluster" ]; then
+    echo "cluster is not on-demand"
     exit 1
   fi
-  smi=$(shipctl get_resource_version_key smi_state "smi")
+  clusterId=$(echo cluster | jq '.id')
+  smi=$(shipctl get_resource_version_key $smiStateResource "smi")
   echo "=== GETting $smi's runtimeTemplateId ==="
   runtimeTemplateId=$(curl -H "Authorization: apiToken $apiToken" $apiUrl/systemMachineImages | jq '.[] | select(.name=="'$smi'") | .runtimeTemplateId')
   echo "$smi's runtimeTemplateId $runtimeTemplateId"
-  echo "=== PUTting $subscriptionId's default cluster $defaultClusterId's runtimeTemplateId = $runtimeTemplateId ==="
-  update=$(curl -X PUT $apiUrl/clusters/$defaultClusterId -H "Authorization: apiToken $apiToken" -H 'content-type: application/json' -d "{\"runtimeTemplateId\": $runtimeTemplateId}")
+  echo "=== PUTting cluster $clusterId's runtimeTemplateId = $runtimeTemplateId ==="
+  update=$(curl -X PUT $apiUrl/clusters/$clusterId -H "Authorization: apiToken $apiToken" -H 'content-type: application/json' -d "{\"runtimeTemplateId\": $runtimeTemplateId}")
   echo "Updated $clusterId: $(echo $update | jq '.')"
 }
 
